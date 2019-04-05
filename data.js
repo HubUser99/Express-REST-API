ad735eeb579132ba02ac750d043c37a543783a16
HubUser99 <cliff.goga@gmail.com>
2019-04-05 14:07:30 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
+// this will be our data base's data structure 
+const DataSchema = new Schema(
+{
+	id: { type: Number, required: true, index: { unique: true } },
+	email: { type: String, required: true, index: { unique: true } },
+	username: { type: String, required: true, index: { unique: true } },
+	isVerified: { type: Boolean, default: false },
+	password: { type: String, required: true },
+	passwordResetToken: { type: String },
+	passwordResetExpires: { type: Date },
+	session_id: { type: String, default: null },
+	active: { type: Boolean, default: false }
+},
+{ timestamps: true }
+);
+
+DataSchema.pre('save', function(next) {
+	const user = this;
+	// only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+  	if (err) return next(err);
+
+      // hash the password along with our new salt
+      bcrypt.hash(user.password, salt, function(err, hash) {
+      	if (err) return next(err);
+
+          // override the cleartext password with the hashed one
+          user.password = hash;
+          next();
+      });
+  });
+});
+
+DataSchema.methods.comparePassword = function(candidatePassword, cb) {
+	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+		if (err) return cb(err);
+		cb(null, isMatch);
+	});
+};
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("Data", DataSchema);
---
+++ server.js
@@ -0,0 +1,195 @@
+const pass = require("./credentials").pass;
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const Data = require("./data");
+const Token = require("./token");
+const assert = require('assert');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
+const validator = require('validator');
+const API_PORT = 3001;
+const app = express();
+const router = express.Router();
+
+// this is MongoDB database
+// connects back end code with the database
+mongoose.connect(
+	pass,
+	{ useNewUrlParser: true }
+	);
+
+let db = mongoose.connection;
+
+db.once("open", () => console.log("connected to the database"));
+
+// checks if connection with the database is successful
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// (optional) only made for logging and
+// bodyParser, parses the request body to be a readable json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger("dev"));
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+app.use((req, res, next) => {
+	console.log(req.connection.remoteAddress);
+	next();
+});
+
+/*
+// this is get method
+// this method fetches all available data in our database
+router.get("/getData", (req, res, next) => {
+	Data.find((err, data) => {
+		if (err) return res.json({ success: false, error: err });
+		return res.json({ success: true, data: data });
+	});
+});
+*/
+
+router.get("/getLast", (req, res, next) => {
+	Data.
+		findOne({}).
+		sort({ id: -1 }).
+		select({ username: 1, id: 1 }).
+		exec(function (err, data) {
+			if (err) return res.json({ success: false, error: err });
+			console.log(data);
+			if (data)
+				return res.json({ success: true, username: data.username, id: data.id });
+			else
+				return res.json({ success: true, username: "None", id: -1 });
+		});
+});
+
+// this is create method
+// this method adds new data in our database
+router.post("/putData", (req, res, next) => {
+	let data = new Data();
+
+	const { id, email, username, password } = req.body;
+
+	console.log(id, email, username, password);
+
+	if ((!id && id !== 0) || !email || !username || !password) {
+		return res.json({
+			success: false,
+			error: "INVALID INPUTS"
+		});
+	}
+
+	if (!validator.isEmail(email)) {
+		return res.json({
+			success: false,
+			error: "INVALID EMAIL"
+		});
+	}
+
+	data.username = username;
+	data.email = email;
+	data.password = password;
+	data.id = id;
+	data.save(err => {
+		if (err) return res.json({ success: false, error: err });
+
+		const token = new Token({_userId: data._id, token: crypto.randomBytes(16).toString('hex')});
+
+		token.save(err => {
+			if (err) return res.json({ success: false, error: err });
+			const transporter = nodemailer.createTransport({
+				service: 'Sendgrid',
+				auth: {user: process.env.SENDGRID_USERNAME, pass: process.env.SENDGRID_PASSWORD}
+			});
+			const mailOptions = {
+				from: 'no-reply@usercount.com',
+				to: data.email,
+				subject: 'Account Verification Token',
+				text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/api\/confirmation\/' + token.token + '.\n'
+			};
+			transporter.sendMail(mailOptions, err => {
+				if (err) return res.json({ success: false, error: err });
+				res.json({ text: 'A verification email has been sent to ' + data.email + '.' });
+			});
+		});
+	});
+});
+
+router.get("/confirmation/:token", (req, res, next) => {
+	console.log(req.params);
+	Token.findOne({token: req.params.token}, (err, token) => {
+		if (!token) return res.status(400).send({ type: 'not-verified', msg: 'We were unable to find a valid token. Your token may have expired.' });
+		Data.findOne({_id: token._userId}, (err, data) => {
+			if (!data) return res.status(400).send({ msg: 'We were unable to find a user for this token.' });
+			if (data.isVerified) return res.status(400).send({ type: 'already-verified', msg: 'This user has already been verified.' });
+
+			data.isVerified = true;
+			data.save(function (err) {
+				if (err) { return res.status(500).send({ msg: err.message }); }
+				res.status(200).send("The account has been verified. Please log in.");
+			});
+		});
+	});
+});
+
+router.post("/validate", (req, res, next) => {
+	let data = new Data();
+
+	const { username, password } = req.body;
+	console.log(username + "  " + password);
+
+	Data.findOne({ username }, (err, user) => {
+		if (err) throw err;
+		
+		if (!user.isVerified) {
+			return res.json({ valid: false, error: "Authorization failed." });
+		}
+
+		if (user) {
+			user.comparePassword(password, function(err, isMatch) {
+				if (err) throw err;
+				console.log(password, isMatch);
+				user.active = true;
+				crypto.randomBytes(16, (err, buffer) => {
+					user.session_id = buffer.toString('hex');
+					user.save(err => {
+						return res.json({ valid: isMatch, username: user.username, session_id: user.session_id, error: "Authorization failed." });
+					});
+				});
+			});
+		} else {
+			return res.json({ valid: false, error: "Authorization failed." });
+		}
+	});
+});
+
+router.post("/logout", (req, res, next) => {
+	const session_id = req.body.session_id;
+	Data.findOne({ session_id: session_id }, (err, user) => {
+		if (err) return res.status(400).send(err);
+		if (user.active) {
+			if (session_id === user.session_id && user.session_id !== "") {
+				user.active = false;
+				user.session_id = null;
+				user.save(err => {
+					if (err) throw err;
+					return res.status(200).send("You have been logged out.");
+				});
+			}
+		} else {
+			return res.status(403).send("User is not logged in.");
+		}
+	});
+});
+
+// append /api for our http requests
+app.use("/api", router);
+
+// launch our backend into a port
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
